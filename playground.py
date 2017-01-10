799a6a7b776faab03cc4ac03521b379c774e7a04
PDHS lab <pdhslab@DSUSD.K12.CA.US>
2017-01-10 10:50:32 -0800

    scrollbar was last thing

--- playground.py
+++ playground.py
@@ -1,5 +1,6 @@
 from Tkinter import * #gives us access to everything in the Tkinter class
 import tkMessageBox
+from PIL import Image, ImageTk
 
 def buttonpress():
     entrytxt = entry1.get()
@@ -8,16 +9,51 @@ def buttonpress():
     
 def addtolist():
     entrytxt = entry1.get()
-    listbox1.insert(END, entrytxt)
-    
+    if check4dup() == False:
+        listbox1.insert(END, entrytxt)
+        findsize()
+    entry1.delete(0,END)
+        
 def addtolist2(event):
     entrytxt = entry1.get()
-    listbox1.insert(END, entrytxt)
-    entry1.delete(0, END)
+    if check4dup() == False:
+        listbox1.insert(END, entrytxt)
+        findsize()
+    entry1.delete(0,END)
     
 def clearlist(event):
-    print "mouse click"
     listbox1.delete(0, END)
+    findsize()
+    
+def check4dup():
+    names = listbox1.get(0, END)
+    if entry1.get() in names:
+        return True
+    else:
+        return False
+        
+def openFileR():
+    clearlist1()
+    f = open("Readme.txt", "r")
+    for line in f:
+        name = line[0:-1]
+        listbox1.insert(END, name)
+    f.close()
+    findsize()
+    
+def openFileW():
+    f = open("Readme.txt", 'w')
+    names = listbox1.get(0, END)
+    for i in names:
+        f.write(i+"\n")
+    f.close()
+        
+def findsize():
+    label1.config(text=listbox1.size())
+    
+def clearlist1():
+    listbox1.delete(0, END)
+    findsize()
 
 root = Tk() #gives us a blank canvas object to work with
 root.title("MWales_First GUI Program with Tkinter")
@@ -32,12 +68,35 @@ entry1.bind("<Return>", addtolist2)
 label1 = Label(root, text="Hello World", bg="purple", anchor=W)
 label1.grid(row=0, column=0, sticky=EW, columnspan=2)
 
-listbox1 = Listbox(root)
+scrollbar = Scrollbar(root, orient=VERTICAL)
+listbox1 = Listbox(root, yscroll.command=scrollbar.set)
+listbox1.config(command=listbox1.yview)
+scrollbar.grid(row=2, column=2,rowspan-10)
 listbox1.grid(row=2, column=0, columnspan=2, sticky=EW)
 listbox1.bind("<Button-3>", clearlist)
 
-listbox1.insert(END, "Harry")
-listbox1.insert(END, "Ron")
-listbox1.insert(END, "Hermione")
+#listbox1.insert(END, "Harry")
+#listbox1.insert(END, "Ron")
+#listbox1.insert(END, "Hermione")
+
+findsize()
+
+image = Image.open("ball.jpg")
+image = image.resize((100,100))
+photo = ImageTk.PhotoImage(image)
+
+label2 = Label(image=photo)
+label2.image = photo #keep a reference!
+label2.grid(row=12, column=0)
+
+menubar = Menu(root)
+filemenu = Menu(menubar, tearoff=0)
+filemenu.add_command(label="Open", command=openFileR)
+filemenu.add_separator()
+filemenu.add_command(label="Save", command=openFileW)
+
+menubar.add_cascade(label="file", menu=filemenu)
+
+root.config(menu=menubar)
 
 mainloop() #causes the windows to display on the screen until program closes
