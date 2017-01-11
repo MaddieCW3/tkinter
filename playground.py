199c1f73064b99ad28576bf4e80ff67e7da1e574
PDHS lab <pdhslab@DSUSD.K12.CA.US>
2017-01-11 11:15:31 -0800

    threads and time

--- playground.py
+++ playground.py
@@ -1,6 +1,8 @@
 from Tkinter import * #gives us access to everything in the Tkinter class
 import tkMessageBox
 from PIL import Image, ImageTk
+from datetime import date, datetime
+from threading import *
 
 def buttonpress():
     entrytxt = entry1.get()
@@ -54,6 +56,19 @@ def findsize():
 def clearlist1():
     listbox1.delete(0, END)
     findsize()
+    
+d = datetime.now()
+y = d.year
+h = d.hour
+
+def generate():
+    while(1):
+        print "Hello"
+        
+thread1 = Thread(target=generate)
+#thread1.start()
+
+#generate()
 
 root = Tk() #gives us a blank canvas object to work with
 root.title("MWales_First GUI Program with Tkinter")
@@ -66,18 +81,18 @@ entry1.grid(row=1, column=0)
 entry1.bind("<Return>", addtolist2)
 
 label1 = Label(root, text="Hello World", bg="purple", anchor=W)
-label1.grid(row=0, column=0, sticky=EW, columnspan=2)
+label1.grid(row=0, column=0, sticky=EW, columnspan=3)
 
 scrollbar = Scrollbar(root, orient=VERTICAL)
-listbox1 = Listbox(root, yscroll.command=scrollbar.set)
-listbox1.config(command=listbox1.yview)
-scrollbar.grid(row=2, column=2,rowspan-10)
+listbox1 = Listbox(root, yscrollcommand=scrollbar.set)
+scrollbar.config(command=listbox1.yview)
+scrollbar.grid(row=2, column=2, rowspan=10, sticky=NS)
 listbox1.grid(row=2, column=0, columnspan=2, sticky=EW)
 listbox1.bind("<Button-3>", clearlist)
 
-#listbox1.insert(END, "Harry")
-#listbox1.insert(END, "Ron")
-#listbox1.insert(END, "Hermione")
+listbox1.insert(END, "Harry")
+listbox1.insert(END, "Ron")
+listbox1.insert(END, "Hermione")
 
 findsize()
 
